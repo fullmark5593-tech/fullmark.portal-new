5170aa40a427c176765961423980ef9ab8d9f582
fullmark5593-tech <fullmark5593@gmail.com>
2026-07-18 03:14:17 +0300

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,11 +1,9 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+// ==========================================================
+// إعدادات Firebase - مشروع fullmark-portal-new
+// ==========================================================
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyBjwzOpxOzkXpMIDRIfeND74YHImUVjEKs",
   authDomain: "fullmark-portal-new.firebaseapp.com",
@@ -16,6 +14,5 @@ const firebaseConfig = {
   measurementId: "G-8LSC9QRD23"
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+export const db = getFirestore(app);
